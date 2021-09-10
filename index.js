e0d0fef26e4a65254ac9a3e8da9f9d179768fe74
tonyhuynh6789 <duc_thinh_03@yahoo.com>
2021-09-10 14:21:52 -0700

    update function added

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ function addItemsToDom(item) {
             <span class = "description">${item.attributes.description}</span>
     </li>   
             <button class = "delete" data-id = "${item.id}">Delete</button>
+            <button class = "update" data-id = "${item.id}">Update</button>
     </div>
     `
 }
@@ -94,14 +95,92 @@ function deleteItem(id) {
 }
 
 
+function addUpdatedItemField(id){
+
+    let item = document.querySelector(`#item-${id} li`)
+    let name = item.querySelector('.name').innerText
+    let description = item.querySelector('.description').innerText
+    let price = item.querySelector('.price').innerText
+
+
+
+    let updateForm = `
+    <input type="number" value="${price}" name="price" id="update-price-${id}">
+    <input type="text" name="name" value="${name}" id="update-name-${id}">
+    <input type="text" name="description" value="${description}" id="update-description-${id}">
+    `
+
+
+    let formDiv = document.createElement('div')
+        formDiv.id = `update-form-${id}`
+        formDiv.innerHTML = updateForm
+        item.append(formDiv)    
+        // debugger
+
+}
+
+
+function sendPatchRequest(id) {
+
+    let updatedPrice = document.getElementById(`update-price-${id}`)
+    let updatedName = document.getElementById(`update-name-${id}`)
+    let updatedDescription = document.getElementById(`update-description-${id}`) 
+    
+    let newItemObject = {
+        name: updatedName.value,
+        price: updatedPrice.value,
+        description: updatedDescription.value
+    }
+
+    let configObj = {
+        method: "PATCH", 
+        headers: {
+            "Content-Type": "application/json",
+            "Accepts": "application/json"
+        },
+        body: JSON.stringify(newItemObject)
+    }
+
+
+    fetch(`http://localhost:3000/items/${id}`, configObj) 
+        .then(resp => resp.json())
+        .then(resp => updateItemOnDom(resp.data))
+
+
+
+     // Remove form   
+    let form = document.getElementById(`update-form-${id}`)
+    form.remove()
+
+}
+
+
+function updateItemOnDom(item) {
+    let liItem  = document.querySelector(`#item-${item.id} li`)
+    liItem.querySelector('.price').innerText = item.attributes.price
+    liItem.querySelector('.name').innerText = item.attributes.name 
+    liItem.querySelector('.description').innerText = item.attributes.description
+
+    // debugger;
+
+}
 
 function handlerClick(e) {
-    // debugger
     if(e.target.className === "delete") {
         let id = e.target.dataset.id
         deleteItem(id)
-        // debugger;
+    } else if (e.target.className === "update") {
+        let itemId = e.target.dataset.id
+        e.target.className = "save"
+        e.target.innerText = "Save"
+        addUpdatedItemField(itemId)
+    } else if (e.target.className === "save") {
+        let id = e.target.dataset.id 
+        e.target.className = "update"
+        e.target.innerText = "Update"
+        sendPatchRequest(id)
     }
+ 
 }
 
 
@@ -113,6 +192,7 @@ document.addEventListener("DOMContentLoaded", function() {
     fetchItems()
     itemForm.addEventListener("submit", handleForm)
     itemList.addEventListener("click", handlerClick)
+
     
 })
 
